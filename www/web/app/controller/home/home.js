106944efcc7592b6f171fcf1a33695c7f6d973a5
ajaxj <ajaxj@qq.com>
2019-09-22 12:42:22 +0800

    'add'

--- www/web/app/controller/home/home.js
+++ www/web/app/controller/home/home.js
@@ -14,18 +14,20 @@ class HomeController extends Controller {
     // let result = await this.ctx.service.eventdetail.list();
 
     //初始数据：
-    let event1 = {"name":"event1"};
-    await ctx.service.event.create(event1);
-    let event2 = {"name":"event2"};
-    await ctx.service.event.create(event2);
+    // let event1 = {"name":"event1"};
+    // await ctx.service.event.create(event1);
+    // let event2 = {"name":"event2"};
+    // await ctx.service.event.create(event2);
+    //
+    // let detail1 = {"title":"event1-title1","eid":1};
+    // await ctx.service.eventdetail.create(detail1);
+    //
+    // let detail = {"title":"event1-title2","eid":1};
+    // await ctx.service.eventdetail.create(detail);
+    // detail = {"title":"event2-title1","eid":2};
+    // await ctx.service.eventdetail.create(detail);
 
-    let detail1 = {"title":"event1-title1","eid":1};
-    await ctx.service.eventdetail.create(detail1);
-
-    let detail = {"title":"event1-title2","eid":1};
-    await ctx.service.eventdetail.create(detail);
-    detail = {"title":"event2-title1","eid":2};
-    await ctx.service.eventdetail.create(detail);
+    let m1 = {"name":"name1","username":"username1","passwd":"passwor"}
 
     ctx.body = "ok";
   }
